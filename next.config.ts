d97ee00bf2239d4e021d2f093258620dda6860d4
baikong <baikong@baikongdeMac-mini.local>
2026-04-10 23:03:49 +0800

    fixed bug

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   basePath: "/severance-calculator",
+  turbopack: {
+    root: import.meta.dirname,
+  },
 };
 
 export default nextConfig;
